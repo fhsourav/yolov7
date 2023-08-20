cfa46711671883506713bc82dd307daf79a2d618
fahim <69208613+fhsourav@users.noreply.github.com>
2023-08-21 02:26:24 +0600

    utils/plots.py - added code for OpenCV blur on inference

--- utils/plots.py
+++ utils/plots.py
@@ -59,7 +59,15 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=3):
     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+
+    # blur
+    x,y,w,h=int(x[0]), int(x[1]), int(x[2] - x[0]), int(x[3] - x[1])
+    ROI = img[y:y+h, x:x+w]
+    blur = cv2.blur(ROI, (99, 99))
+    img[y:y+h, x:x+w] = blur
+    # blur
+
+    # cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA) # uncomment when not blurring
     if label:
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
